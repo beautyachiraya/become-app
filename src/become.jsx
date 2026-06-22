da073e47f61f2643de3747e8df4a468d76e251ce
beautyachiraya <achirayawattanakit@gmail.com>
2026-06-23 01:09:28 +0400

    Update become.jsx

--- src/become.jsx
+++ src/become.jsx
@@ -2,7 +2,7 @@ import { useState, useMemo, useRef, useEffect } from "react";
 import { auth } from "./firebase";
 import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const googleProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider(); googleProvider.setCustomParameters({prompt:"select_account"});
 const B = {
   caramel:"#B4915F", caramelDk:"#8C6E40", caramelLt:"#D4B080",
   ivory:"#FAF7F2", ivoryDk:"#F5EFE6", linen:"#EDE5D8",
