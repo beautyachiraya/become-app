b7bc592383b15349f961b1078b82dd218c69b902
beautyachiraya <achirayawattanakit@gmail.com>
2026-06-23 01:34:28 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"; import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBnN0Skhq_y2azpES-ccv_yv9voxa2JyXg",
@@ -12,4 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth(app); export const db = getFirestore(app);
