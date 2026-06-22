4fc7093feffbf8b9b5fbb5223e29deab8b29d77e
beautyachiraya <achirayawattanakit@gmail.com>
2026-06-22 23:40:42 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  projectId: "xxx",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "xxx"
+  apiKey: "AIzaSyBnN0Skhq_y2azpES-ccv_yv9voxa2JyXg",
+  authDomain: "become-app-dde78.firebaseapp.com",
+  projectId: "become-app-dde78",
+  storageBucket: "become-app-dde78.firebasestorage.app",
+  messagingSenderId: "747108494298",
+  appId: "1:747108494298:web:af546e90d9d70294856ee3",
+  measurementId: "G-9E4YEKWZD8"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
